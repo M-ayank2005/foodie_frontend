c8ffea6bd0a8a63d76137cd8ac42206055044e71
Mayank Mishra <mrmishramayank2005@gmail.com>
2024-08-25 15:02:24 +0530

    fst

---
+++ src/app/page.js
@@ -0,0 +1,67 @@
+'use client';
+import React from "react";
+import Navbar from "@/Components/Navbar";
+import Footer from "@/Components/footer";
+import Carousel from "@/Components/Corousel";
+import Card from "@/Components/card";
+import image1 from "../lib/pizza.jpg";
+import image2 from "../lib/pizza2.jpg";
+import image3 from "../lib/fries.jpg";
+import image4 from "../lib/chow.jpg";
+import image5 from "../lib/berger.jpg";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="pt-0 mb-5">
+      <Navbar />
+      </div>
+     
+      <div className="w-full h-1/3 p-5  pt-0 mt-5">
+        <Carousel />
+      </div>
+
+      <div className="text-bold  bg-gray-100 rounded-2xl p-3 pl-10 w-full">
+        <p className="text-3xl font-bold text-left">Hot & Spicy ....</p>
+      </div>
+
+      <div className="p-5 m-5">
+        <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <Card
+            imageSrc={image1}
+            title="Delicious Pizza"
+            description="A tasty pizza with fresh ingredients."
+            link="/pizza"
+          />
+          <Card
+            imageSrc={image2}
+            title="Delicious Burger"
+            description="A tasty burger with fresh ingredients."
+            link="/burger"
+          />
+          <Card
+            imageSrc={image3}
+            title="Tasty French Fries"
+            description="Tasty french fries with fresh ingredients."
+            link="/fries"
+          />
+          <Card
+            imageSrc={image4}
+            title="Delicious Chowmein"
+            description="A tasty chowmein made with fresh ingredients."
+            link="/chowmein"
+          />
+          <Card
+            imageSrc={image5}
+            title="Delicious Burger"
+            description="A tasty burger with fresh ingredients."
+            link="/burger"
+          />
+        </div>
+      </div>
+
+      <Footer />
+    </main>
+  );
+}
